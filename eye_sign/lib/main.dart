aebf4cb8272ff300250fdeff39fc35146752ed86
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2018-11-24 15:41:16 -0500

    reset

--- eye_sign/lib/main.dart
+++ eye_sign/lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
