0d0582ba545707a49a8b4baf84f061332945cef5
Jake <jbildy@uwo.ca>
2018-11-25 08:02:54 -0500

    added new method for returning specific letter

--- eye_sign/android/app/src/main/java/com/example/eyesign/MainActivity.java
+++ eye_sign/android/app/src/main/java/com/example/eyesign/MainActivity.java
@@ -5,6 +5,11 @@ import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 import org.tensorflow.lite.Interpreter;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +21,7 @@ public class MainActivity extends FlutterActivity {
   String modelFile="asl.lite";
   Interpreter tflite;
 
-  private MappedByteBuffer loadModelFile(Activity activity,String MODEL_FILE) throws IOException {
+  private MappedByteBuffer loadModelFile(Activity activity, String MODEL_FILE) throws IOException {
 
     AssetFileDescriptor fileDescriptor = activity.getAssets().openFd(MODEL_FILE);
     FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
@@ -44,4 +49,39 @@ public class MainActivity extends FlutterActivity {
 
     return out;
   }
+
+  public String getASL(String imgpath)
+  {
+    float[] letter = runTensorflow(imgpath);
+    int biggest = 0;
+
+    for (int i = 0; i<letter.length;i++)
+    {
+
+      if (letter[i] > letter[biggest])
+      {
+        biggest = i;
+      }
+    }
+
+    switch (biggest){
+      case 0:
+        return "a";
+      break;
+      case 1:
+        return "b";
+      break;
+      case 2:
+        return "c";
+      break;
+      case 3:
+        return "d";
+      break;
+      default:
+        return "this letter isn't added yet!";
+      break;
+    }
+  }
+
+
 }
